497747eb08413ce4671f54b0af9958041d169ff9
Usama Rahman <rahmanusama390@gmail.com>
2023-10-04 22:52:43 +0600

    making

---
+++ app.js
@@ -0,0 +1,10 @@
+const from = document.querySelector("from");
+
+from.addEventListener('submit',function(e){
+    e.preventDefault()
+
+const height = parseInt(document.querySelector("#height").value)
+const weight = parseInt(document.querySelector("#weight").value)
+
+
+})
